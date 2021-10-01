23e7b58f8afe4444e77b8aa07abb693e13dab9fa
robertgaspar <gaspar.robert7@gmail.com>
2021-10-01 13:47:32 -0500

    completed assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a,b) {
+    return a + b;
+}
+function subtract(a,b) {
+    return a - b;
+}
+function multiply(a,b) {
+    return a * b;
+}
+function divide(a,b) {
+    return a / b;
+}
+function increment(a) {
+    return a + 1;
+}
+function decrement(a) {
+    return a - 1;
+}
+function makeInt(n) {
+    return parseInt(n);
+}
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+function makeInt(string) {
+    return parseFloat(string, 10) 
+}
+function preserveDecimal(string) {
+    return parseFloat(string, 2.222);
+}
